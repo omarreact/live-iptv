997625dcd87fc7b944e5e2fda23c3245be9a2a69
MD OMAR FARUK KHAN <91119648+omarreact@users.noreply.github.com>
2026-09-20 17:22:43 +0600

    Fix Cineplex UI type import

--- src/components/cineplex-library.tsx
+++ src/components/cineplex-library.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { FormEvent, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import type { FormEvent } from "react";
 import {
   Clapperboard,
   ExternalLink,
